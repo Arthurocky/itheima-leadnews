882e852e8017fa8f607b4a304347438614a3c7c5
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-09 01:00:25 +0800

    ✨ 登录功能实现

---
+++ itheima-leadnews-service/itheima-leadnews-service-admin/src/main/java/com/itheima/admin/mapper/AdUserMapper.java
@@ -0,0 +1,10 @@
+package com.itheima.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.admin.pojo.AdUser;
+
+/**
+ * @author Arthurocky
+ */
+public interface AdUserMapper extends BaseMapper<AdUser> {
+}
