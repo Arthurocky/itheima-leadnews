fd133e916ba0f95c3403366ecac1f42cb460ea51
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-09 22:10:47 +0800

    ⬆️ 升级依赖并准备实名认证、文章和媒体的基本框架

---
+++ itheima-leadnews-service/itheima-leadnews-service-user/src/main/java/com/itheima/user/service/impl/ApUserRealnameServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.user.service.impl;
+
+import com.itheima.user.pojo.ApUserRealname;
+import com.itheima.user.mapper.ApUserRealnameMapper;
+import com.itheima.user.service.ApUserRealnameService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * APP实名认证信息表 服务实现类
+ * </p>
+ *
+ * @author ljh
+ * @since 2021-12-22
+ */
+@Service
+public class ApUserRealnameServiceImpl extends ServiceImpl<ApUserRealnameMapper, ApUserRealname> implements ApUserRealnameService {
+
+}
