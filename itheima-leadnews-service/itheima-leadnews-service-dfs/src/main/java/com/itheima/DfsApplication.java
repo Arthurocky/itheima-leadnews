c2f4d62c16f312cedb287e90355562ee380af47e
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-12 16:19:01 +0800

    ✨ 实现图片上传功能

--- itheima-leadnews-service/itheima-leadnews-service-dfs/src/main/java/com/itheima/DfsApplication.java
+++ itheima-leadnews-service/itheima-leadnews-service-dfs/src/main/java/com/itheima/DfsApplication.java
@@ -1,8 +1,10 @@
 package com.itheima;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 
 /**
@@ -10,6 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+//可以省略，DFS中已经配置了
+@Import(FdfsClientConfig.class)
 public class DfsApplication {
     public static void main(String[] args) {
         SpringApplication.run(DfsApplication.class, args);
