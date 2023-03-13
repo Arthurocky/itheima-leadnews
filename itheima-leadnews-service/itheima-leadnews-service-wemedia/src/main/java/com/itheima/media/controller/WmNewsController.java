6cd610f00eee3eb0757f9decd709063e4a19be1b
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-13 19:48:31 +0800

    ✨ 自媒体文章分页查询

--- itheima-leadnews-service/itheima-leadnews-service-wemedia/src/main/java/com/itheima/media/controller/WmNewsController.java
+++ itheima-leadnews-service/itheima-leadnews-service-wemedia/src/main/java/com/itheima/media/controller/WmNewsController.java
@@ -1,8 +1,14 @@
 package com.itheima.media.controller;
 
 
+import com.itheima.common.pojo.PageInfo;
+import com.itheima.common.pojo.PageRequestDto;
+import com.itheima.common.pojo.Result;
+import com.itheima.media.dto.WmNewsDto;
 import com.itheima.media.pojo.WmNews;
 import com.itheima.media.service.WmNewsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.swagger.annotations.Api;
@@ -12,8 +18,7 @@ import com.itheima.core.controller.AbstractCoreController;
 /**
 * <p>
 * 自媒体图文内容信息表 控制器</p>
-* @author ljh
-* @since 2021-12-22
+* @author Arthurocky
 */
 @Api(value="自媒体图文内容信息表",tags = "WmNewsController")
 @RestController
@@ -27,6 +32,17 @@ public class WmNewsController extends AbstractCoreController<WmNews> {
     public WmNewsController(WmNewsService wmNewsService) {
         super(wmNewsService);
         this.wmNewsService=wmNewsService;
+    }
+
+    /**
+     * 分页查询
+     * @param pageRequestDto
+     * @return
+     */
+    @PostMapping("/searchPage")
+    public Result<PageInfo<WmNews>> findByPageDto(@RequestBody PageRequestDto<WmNewsDto> pageRequestDto){
+        PageInfo<WmNews> pageInfo = wmNewsService.findByPageDto(pageRequestDto);
+        return Result.ok(pageInfo);
     }
 
 }
