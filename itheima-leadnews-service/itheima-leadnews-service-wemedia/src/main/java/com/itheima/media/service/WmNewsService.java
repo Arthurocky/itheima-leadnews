c126d849534e3d86b6dae3b30a4f5c832ce11153
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-13 20:42:19 +0800

    ✨ 自媒体文章--发布、修改，保存草稿

--- itheima-leadnews-service/itheima-leadnews-service-wemedia/src/main/java/com/itheima/media/service/WmNewsService.java
+++ itheima-leadnews-service/itheima-leadnews-service-wemedia/src/main/java/com/itheima/media/service/WmNewsService.java
@@ -3,6 +3,7 @@ package com.itheima.media.service;
 import com.itheima.common.pojo.PageInfo;
 import com.itheima.common.pojo.PageRequestDto;
 import com.itheima.media.dto.WmNewsDto;
+import com.itheima.media.dto.WmNewsDtoSave;
 import com.itheima.media.pojo.WmNews;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -23,4 +24,10 @@ public interface WmNewsService extends IService<WmNews> {
      */
     PageInfo<WmNews> findByPageDto(PageRequestDto<WmNewsDto> pageRequestDto);
 
+    /**
+     * 保存自媒体文章 保存草稿 和 添加 或者修改
+     * @param wmNewsDtoSave
+     * @param isSubmit
+     */
+    WmNews save(WmNewsDtoSave wmNewsDtoSave, Integer isSubmit);
 }
